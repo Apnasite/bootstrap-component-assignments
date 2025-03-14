482024bc683ff9e224499449b88e42bd75cbd56e
Vilas Shetkar <vilasshetkar@gmail.com>
2025-03-14 14:23:08 +0530

    Update script.js

--- SubmittedAssignments/whatsapp/script.js
+++ SubmittedAssignments/whatsapp/script.js
@@ -1,49 +1,48 @@
 class WhatsAppDashboard extends HTMLElement {
   constructor() {
     super();
-    this.attachShadow({ mode: "open" });
     this.instances = [
       {
-        barnd:"Happy home ",
+        barnd: "Happy home ",
         instanceEndPoint: "https://whatsapp1.apnasite.in/api",
         token: "$2b$10$tN1bljzsVQj79UUIODheZ.rsiLmzbFDjT4fu4SE6PntPj1.yYJjwW",
         instanceName: "hh",
-         mobile:"8600006783"
+        mobile: "8600006783"
       },
       {
-        barnd:"Happy home ",
+        barnd: "Happy home ",
         instanceEndPoint: "https://whatsapp2.apnasite.in/api",
         token: "$2b$10$K5jrVqoHSyDxr9SCmmZybO52whYBqnwBe_5.hPSqzfchQc.Vm68m6",
         instanceName: "hh",
-         mobile:"8390055038"
+        mobile: "8390055038"
       },
       {
-        barnd:"Happy home ",
+        barnd: "Happy home ",
         instanceEndPoint: "https://whatsapp3.apnasite.in/api",
         token: "$2b$10$dHWv_GQv3FnADD60z5ZibOZvIpRNRGf2SOBvI.p658Mr2xjh1UiVm",
         instanceName: "hh",
-        mobile:"8180806337"
+        mobile: "8180806337"
       },
       {
-        barnd:"Apnaguru",
+        barnd: "Apnaguru",
         instanceEndPoint: "https://whatsapp6.apnasite.in/api",
         token: "$2b$10$BCkpOb3Nctb2.CldiKoyiO.J1JlRvQZP5IhNAEZfAC6q4L9aJ8Viy",
         instanceName: "apnaguru2",
-         mobile:"9049508514"
+        mobile: "9049508514"
       },
       {
-        barnd:"Apnaguru",
+        barnd: "Apnaguru",
         instanceEndPoint: "https://whatsapp10.apnasite.in/api",
         token: "$2b$10$R3hCHf1DwvojbZwYc8iSP.QUen5Bq72aGmhIRPceVqv5PKE6jqoZO",
         instanceName: "apnaguru6",
-        mobile:"7840974679"
+        mobile: "7840974679"
       },
       {
-        barnd:"Apnasite",
+        barnd: "Apnasite",
         instanceEndPoint: "https://whatsapp4.apnasite.in/api",
         token: "$2b$10$Xt.AdDYeAlsyu0elWxnLx.nbUGMLCGOm_ZQU7Nh7dA6vjB9r1GKPm",
         instanceName: "apnaguru1",
-        mobile:"9175546005"
+        mobile: "9175546005"
       }
     ];
   }
@@ -63,24 +62,26 @@ class WhatsAppDashboard extends HTMLElement {
 
   async startSession(index) {
     const instance = this.instances[index];
-    const confirmStart = confirm("Do you want to start the session?");
-    
-    if (confirmStart) {
-      const response = await fetch(`${instance.instanceEndPoint}/${instance.instanceName}/start-session`, {
-        method: "POST",
-        headers: { 
-          "Content-Type": "application/json", 
-          Authorization: `Bearer ${instance.token}` 
-        },
-        body: JSON.stringify({ webhook: "", waitQrCode: false })
-      });
-      const data = await response.json();
-      alert(`Start Session: ${JSON.stringify(data)}`);
-    } else {
-      alert("Session start canceled.");
-    }
+    const response = await fetch(`${instance.instanceEndPoint}/${instance.instanceName}/start-session`, {
+      method: "POST",
+      headers: { 
+        "Content-Type": "application/json", 
+        Authorization: `Bearer ${instance.token}` 
+      },
+      body: JSON.stringify({ webhook: "", waitQrCode: false })
+    });
+    const data = await response.json();
+    alert(`Start Session: ${JSON.stringify(data)}`);
   }
-  
+
+  async restartSession(index) {
+    const instance = this.instances[index];
+    await fetch(`${instance.instanceEndPoint}/${instance.instanceName}/close-session`, {
+      method: "POST",
+      headers: { Authorization: `Bearer ${instance.token}` }
+    });
+    setTimeout(() => this.startSession(index), 2000);
+  }
 
   async sendMessage(index) {
     const instance = this.instances[index];
@@ -101,8 +102,7 @@ class WhatsAppDashboard extends HTMLElement {
   }
 
   render() {
-    this.shadowRoot.innerHTML = `
-      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
+    this.innerHTML = `
       <style>
         .container { padding: 20px; }
         .card { margin-bottom: 20px; }
@@ -116,9 +116,10 @@ class WhatsAppDashboard extends HTMLElement {
                   <h5 class="card-title">${instance.barnd}</h5>
                   <h6 class="card-title">${instance.mobile}</h6>
                   <p class="card-text">Endpoint: ${instance.instanceEndPoint}</p>
-                  <button class="btn btn-primary m-1" onclick="this.getRootNode().host.checkStatus(${index})">Check Status</button>
-                  <button class="btn btn-success m-1" onclick="this.getRootNode().host.startSession(${index})">Start Session</button>
-                  <button class="btn btn-info m-1" onclick="this.getRootNode().host.sendMessage(${index})">Send Message</button>
+                  <button class="btn btn-primary m-1" onclick="document.querySelector('whatsapp-dashboard').checkStatus(${index})">Check Status</button>
+                  <button class="btn btn-success m-1" onclick="document.querySelector('whatsapp-dashboard').startSession(${index})">Start Session</button>
+                  <button class="btn btn-warning m-1" onclick="document.querySelector('whatsapp-dashboard').restartSession(${index})">Restart Session</button>
+                  <button class="btn btn-info m-1" onclick="document.querySelector('whatsapp-dashboard').sendMessage(${index})">Send Message</button>
                 </div>
               </div>
             </div>
